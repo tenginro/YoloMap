f975bc232e978b094df7d03b18285d405e3c791e
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-04-17 21:40:35 -0400

    adding favicon

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
+import logo from "./logo.jpeg";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
-	);
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">
+          <div className="logoLine">
+            <img className="logo" src={logo} alt="icon"></img>
+            Home
+          </div>
+        </NavLink>
+      </li>
+      {isLoaded && (
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
+    </ul>
+  );
 }
 
 export default Navigation;
