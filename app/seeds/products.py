23cba66f71e231a6d73ee3534964c380bc8f5bc6
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-04-19 20:01:19 -0400

    seeded more data

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -45,11 +45,27 @@ def seed_products():
     )
     product6 = Product(
         creatorId=1,
-        placeId=1,
+        placeId=5,
         name="General Admission Ticket",
         description="one day",
         cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/DtW76OIGv5Y3_JMATH50Nw/o.jpg",
         price=27,
+    )
+    product7 = Product(
+        creatorId=1,
+        placeId=6,
+        name="Flight",
+        description="flight from my place, for two",
+        cover_pic="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Transportation_Security_Administration_seal.svg/1200px-Transportation_Security_Administration_seal.svg.png",
+        price=1000,
+    )
+    product8 = Product(
+        creatorId=1,
+        placeId=6,
+        name="Hotel",
+        description="hotel for one week",
+        cover_pic="https://cache.marriott.com/content/dam/marriott-renditions/BOSQU/bosqu-double-superior-6746-hor-wide.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1336px:*",
+        price=1500,
     )
 
     all_products = [
@@ -59,6 +75,8 @@ def seed_products():
         product4,
         product5,
         product6,
+        product7,
+        product8,
     ]
     add_products = [db.session.add(product) for product in all_products]
     db.session.commit()
