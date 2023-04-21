5d8c446419f9b95944bb618dd80b6fa2e0d41b2c
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-04-21 17:12:56 -0400

    updated seed data for pictures

--- app/seeds/places.py
+++ app/seeds/places.py
@@ -6,32 +6,33 @@ from sqlalchemy.sql import text
 def seed_places():
     place1 = Place(
         creatorId=1,
-        name="Alive & Kicking Lobsters",
-        address="269 Putnam Ave",
-        city="Cambridge",
+        name="Jiangnan",
+        address="177 Tremont St",
+        city="Boston",
         state="MA",
-        description="The specialty here is the classic lobster roll, made with succulent chunks of Maine lobster meat, lightly dressed with mayo, and served on a warm and buttery roll. Along with the lobster rolls, you'll also find other delectable seafood options such as shrimp rolls, crab rolls, and clam chowder. ",
-        phone="(617) 876-0451",
-        hours="Mon-Sat: 11AM-6PM; Sun: 11AM-4PM",
+        description="It fills the role of upscale Chinese fine dining. It's located next to Boston Commons and Chinatown with limited parking meters available. The interior inside consists of old upscale European style decoration and art. There's a bar once you come in, then two tables for a large party, and then the main dining area.",
+        website="http://jiangnanny.com",
+        phone="(857) 277-0668",
+        hours="Mon-Fri: 11:30AM-9:30PM (3PM-4:30PM Closed); Sat-Sun: 11:30AM-9:30PM",
         category="Restaurant",
-        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/B35STt7pv-BN8mh0BIAXTg/o.jpg",
-        lat=42.362574,
-        lng=-71.1164871,
+        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/ab642O-Blm-_uE6Dlsx05w/o.jpg",
+        lat=42.3530538,
+        lng=-71.0643608,
     )
     place2 = Place(
-        creatorId=1,
-        name="Konditor Meister",
-        address="32 Wood Rd",
-        city="Braintree",
-        state="MA",
-        description="It is a charming bakery store, where the aroma of freshly baked goods will entice your senses and make your mouth water. The bakery specializes in crafting artisanal bread, pastries, cakes, and desserts that are baked fresh every day using high-quality ingredients. From classic croissants and muffins to decadent cakes and cookies.",
-        website="http://www.konditormeister.com",
-        phone="(781) 849-1970",
-        hours="Mon-Sat: 8AM-6PM; Sun: 8AM-4PM",
-        category="Bakery",
-        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/9-RHkYNqR5hlv38Df2vX9Q/o.jpg",
-        lat=42.2258022,
-        lng=-71.0361444,
+        creatorId=2,
+        name="Cha Redefine",
+        address="507 S Spring St",
+        city="Los Angeles",
+        state="CA",
+        description="milk tea, bubble tea",
+        website="http://www.charedefine.com/",
+        phone="(213) 266-8404",
+        hours="11:30AM-7PM",
+        category="Coffee/Tea",
+        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/DxQ0k_ywz9RPxUbMif1UNA/o.jpg",
+        lat=33.9665211,
+        lng=-118.2724931,
     )
     place3 = Place(
         creatorId=1,
@@ -54,7 +55,7 @@ def seed_places():
         address="1815 Massachusetts Ave",
         city="Cambridge",
         state="MA",
-        description="This gym is equipped with the latest and greatest in exercise equipment, and the expert trainers are here to help you achieve your fitness goals. It offers a wide range of classes, including cardio, strength training, yoga, and Pilates, to help you stay motivated and engaged in your fitness journey. Our clean and spacious facility provides a comfortable environment for working out, and the friendly staff are always available to answer any questions and provide guidance. ",
+        description="This gym is equipped with the latest and greatest in exercise equipment, and the expert trainers are here to help you achieve your fitness goals. It offers a wide range of classes, including cardio, strength training, yoga, and Pilates, to help you stay motivated and engaged in your fitness journey. ",
         website="https://www.planetfitness.com",
         phone="(617) 945-8596",
         hours="Mon-Thu: 6AM-12AM; Fri: 6AM-10PM; Sat-Sun: 7AM-7PM",
@@ -94,7 +95,37 @@ def seed_places():
         lng=-81.490429,
     )
 
-    all_places = [place1, place2, place3, place4, place5, place6]
+    place7 = Place(
+        creatorId=3,
+        name="Disney World",
+        address="Walt Disney World Resort",
+        city="Orlando",
+        state="FL",
+        description="WDW is made up of four theme parks: Magic Kingdom, Epcot, Disney's Hollywood Studios, and Disney's Animal Kingdom. ",
+        website="https://disneyworld.disney.go.com/?CMP=OKC-80007798_GM_WDW_destination_waltdisneyworldresort_NA",
+        phone="(407) 939-5277",
+        hours="Open 24 hours",
+        category="Travel",
+        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/3FENUBmEzXnA_E0eievktA/o.jpg",
+        lat=28.3771857,
+        lng=-81.5733149,
+    )
+    place8 = Place(
+        creatorId=2,
+        name="Alive & Kicking Lobsters",
+        address="269 Putnam Ave",
+        city="Cambridge",
+        state="MA",
+        description="The specialty here is the classic lobster roll, made with succulent chunks of Maine lobster meat, lightly dressed with mayo, and served on a warm and buttery roll. Other delectable seafood options such as shrimp rolls, crab rolls, and clam chowder. ",
+        phone="(617) 876-0451",
+        hours="Mon-Sat: 11AM-6PM; Sun: 11AM-4PM",
+        category="Restaurant",
+        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/B35STt7pv-BN8mh0BIAXTg/o.jpg",
+        lat=42.362574,
+        lng=-71.1164871,
+    )
+
+    all_places = [place1, place2, place3, place4, place5, place6, place7, place8]
     add_places = [db.session.add(place) for place in all_places]
     db.session.commit()
 
--- app/seeds/products.py
+++ app/seeds/products.py
@@ -5,34 +5,34 @@ from sqlalchemy.sql import text
 def seed_products():
     product1 = Product(
         creatorId=1,
-        placeId=1,
+        placeId=8,
         name="Lobster Roll",
         description="lobster sandwich",
-        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/DtW76OIGv5Y3_JMATH50Nw/o.jpg",
+        cover_pic="https://www.foodiecrush.com/wp-content/uploads/2020/06/Lobster-Rolls-foodiecrush.com-020.jpg",
         price=30,
     )
     product2 = Product(
-        creatorId=1,
-        placeId=1,
+        creatorId=3,
+        placeId=8,
         name="Daily Special",
         description="2 cooked lobsters + steamed mussels",
         cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/Yj58WMQfUdmbxlYzUn_UWg/o.jpg",
         price=47,
     )
     product3 = Product(
-        creatorId=1,
+        creatorId=2,
         placeId=2,
-        name="Tiramisu",
-        description="one piece",
-        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/HOQvhAnwuBntwBbmlWUxyA/o.jpg",
-        price=2,
+        name="Milk tea",
+        description="combo",
+        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/iXA4Fmj6vFmLBkCs5U_YIQ/o.jpg",
+        price=30,
     )
     product4 = Product(
         creatorId=1,
         placeId=3,
         name="Oyster",
         description="one dollar oyster",
-        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/_6lnTI6uI3nRM4cfXJG6ww/o.jpg",
+        cover_pic="https://www.washingtonpost.com/resizer/FiEKMAUiSxtyJl6w5JlQ7MtHCxw=/arc-anglerfish-washpost-prod-washpost/public/2L4TYTLHBQQ7TM3DXB2IXTAYKA.jpg",
         price=12,
     )
     product5 = Product(
@@ -67,6 +67,30 @@ def seed_products():
         cover_pic="https://cache.marriott.com/content/dam/marriott-renditions/BOSQU/bosqu-double-superior-6746-hor-wide.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1336px:*",
         price=1500,
     )
+    product9 = Product(
+        creatorId=1,
+        placeId=1,
+        name="Peking Duck",
+        description="Combo for peking duck",
+        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/C9QovNh3xz4Lt7URbFLUDQ/o.jpg",
+        price=100,
+    )
+    product10 = Product(
+        creatorId=3,
+        placeId=7,
+        name="Resort hotel",
+        description="7 days stay",
+        cover_pic="https://i.insider.com/60b91309bee0fc0019d5b2a0?width=700",
+        price=2500,
+    )
+    product11 = Product(
+        creatorId=3,
+        placeId=7,
+        name="General Admission Ticket",
+        description="Annual Pass",
+        cover_pic="https://s3-media0.fl.yelpcdn.com/bphoto/qWXZH5N8lim9USuY1WSP9w/o.jpg",
+        price=1400,
+    )
 
     all_products = [
         product1,
@@ -77,6 +101,9 @@ def seed_products():
         product6,
         product7,
         product8,
+        product9,
+        product10,
+        product11,
     ]
     add_products = [db.session.add(product) for product in all_products]
     db.session.commit()
