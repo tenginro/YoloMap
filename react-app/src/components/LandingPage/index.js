ef83c3b4d712de4a8914831feb810cb4ad9b40e2
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-04-22 16:49:20 -0400

    updating landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -14,13 +14,13 @@ export default function LandingPage() {
   useEffect(() => {
     const intervalFunc = setInterval(() => {
       setIndex((index + 1) % length);
-    }, 3000);
+    }, 5000);
     return () => clearInterval(intervalFunc);
   }, [index, length]);
 
   const backgroundStyle1 = {
     backgroundImage: `url(${allBackgroundImages[index]})`,
-    height: "100vh",
+    height: "70vh",
     width: "100%",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
@@ -28,20 +28,69 @@ export default function LandingPage() {
   };
 
   return (
-    <div className="main-page-container" style={backgroundStyle1}>
-      <h2
-        style={{
-          fontSize: "100px",
-          color: "white",
-          paddingLeft: "100px",
-          fontWeight: "900",
-          zIndex: 2,
-          textShadow: "2px 2px 2px rgba(0, 0, 0, 0.5)",
-        }}
-      >
-        <div>You only live once. </div>
-        <div>So plan it better.</div>
-      </h2>
-    </div>
+    <>
+      <div className="main-page-container" style={backgroundStyle1}>
+        <h2
+          style={{
+            fontSize: "100px",
+            color: "white",
+            paddingLeft: "100px",
+            fontWeight: "900",
+            zIndex: 2,
+            textShadow: "2px 2px 2px rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          <div>You only live once. </div>
+          <div>So plan it better.</div>
+        </h2>
+      </div>
+      <div className="categoriesContainer">
+        <h3>Categories</h3>
+        <div className="allCateContainer">
+          <div
+            className="eachCateContainer"
+            onClick={(e) => alert("Feature coming soon")}
+          >
+            <i className="fa-solid fa-palette fa-2x"></i>
+            <div>Art</div>
+          </div>
+          <div
+            className="eachCateContainer"
+            onClick={(e) => alert("Feature coming soon")}
+          >
+            <i className="fa-solid fa-cake-candles fa-2x"></i>
+            <div>Bakery</div>
+          </div>
+          <div
+            className="eachCateContainer"
+            onClick={(e) => alert("Feature coming soon")}
+          >
+            <i className="fa-solid fa-wine-glass fa-2x"></i>
+            <div>Bar</div>
+          </div>
+          <div
+            className="eachCateContainer"
+            onClick={(e) => alert("Feature coming soon")}
+          >
+            <i className="fa-solid fa-mug-saucer fa-2x"></i>
+            <div>Coffee/Tea</div>
+          </div>
+          <div
+            className="eachCateContainer"
+            onClick={(e) => alert("Feature coming soon")}
+          >
+            <i className="fa-solid fa-utensils fa-2x"></i>
+            <div>Restaurant</div>
+          </div>
+          <div
+            className="eachCateContainer"
+            onClick={(e) => alert("Feature coming soon")}
+          >
+            <i className="fa-solid fa-plane fa-2x"></i>
+            <div>Travel</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
